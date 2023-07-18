f1d6c4a2dd43ec9062848ac8adb78d6c03cd716b
Shanija <shanizmailbox@gmail.com>
2023-07-19 01:59:53 +0400

    simple calculator using js

---
+++ Calculator/calculator.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let screen = document.getElementById("screen");
+
+function btnClick(value) {
+  screen.value += value;
+}
+
+function clearScreen() {
+  screen.value = "";
+}
+
+function findResult() {
+  let result = eval(screen.value);
+  screen.value = result;
+}
